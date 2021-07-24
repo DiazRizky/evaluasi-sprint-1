9e1e70192274f6945d908057e3013730c9da20e8
DiazRizky <86656562+DiazRizky@users.noreply.github.com>
2021-07-24 10:41:20 +0700

    Add files via upload

---
+++ jawaban nomor 4.js
@@ -0,0 +1,51 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Evaluasi Sprint1</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Evaluasi Sprint1</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Evaluasi Sprint1</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Evaluasi Sprint1</title>
+</head>
+<body>
+    <script>
+    alert("Diaz Rizky Purnawan\nMobile (1)")
+for(let i = 60; i < 100; i++){
+    if(i%2==0) {
+    document.write("<p>Angka Genap : " + i + "</p>")
+ }
+}
+</script>
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Evaluasi Sprint1</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Evaluasi Sprint1</title>
+ }
+}
+</script>
+</body>
+</html>
+ }
+}
+</script>
+</body>
+</html>
+ }
+}
+</script>
+</body>
+</html>
